619297bce4a3b6ea9b7680c27e9eccbaf8e31c7d
jjin <jjin@lyft.com>
2022-01-15 13:21:37 -0800

    edit to one turn

--- server/index.js
+++ server/index.js
@@ -9,29 +9,37 @@ const AVATAR = 'AVATAR';
 const CPU = 'CPU';
 
 function getRandomMultiplierWithRange(min, max) { // min and max included 
-    let randNum = Math.floor(Math.random() * (max - min + 1) + min)
-    return randNum / 100
+    let randNum = Math.floor(Math.random() * (max - min + 1) + min);
+    return randNum / 100;
 }
 
-function battle(avatarHp, cpuHp, avatarAttack, cpuAttack, isAvatarTurn) {
-    if (isAvatarTurn) {
-        cpuHp -= avatarAttack * getRandomMultiplierWithRange(70,100);
-    } else {
-        avatarHp -= cpuAttack * getRandomMultiplierWithRange(40,100);
-    }
+function battle(avatarHp, cpuHp, avatarAttack, cpuAttack) {
+    // Make the game more favorable for the avatar
+    var dmgFromAvatar = avatarAttack * getRandomMultiplierWithRange(70,100);
+    var dmgFromCpu = cpuAttack * getRandomMultiplierWithRange(40,100);
 
+    
+    cpuHp -= dmgFromAvatar;
+    avatarHp -= dmgFromCpu;
+
+    // Check if we have winners, the order matters, check CPU's hp first.
+    // If one party won, we need to add back the dmg inflicted for the other party.
+    // This is kind of a hack since we are trying to make 2 turns in one go. Let me know if you have qs
     var whoWon = ''
     if (cpuHp < 0) {
         whoWon = AVATAR;
+        avatarHp += dmgFromCpu;
     } else if (avatarHp < 0) {
         whoWon = CPU;
+        cpuHp += dmgFromAvatar;
     }
 
     var tokenBoostMultiplier = 1;
     var tokenLossMultiplier = 1;
     var xpBoostMultiplier = 1;
     if (whoWon == AVATAR) {
-        tokenBoostMultiplier = getRandomMultiplierWithRange(50,100) + 1
+        // we + 1 for the boosts 
+        tokenBoostMultiplier = getRandomMultiplierWithRange(50,100) + 1;
         xpBoostMultiplier = getRandomMultiplierWithRange(5,10) + 1;
     } else if (whoWon == CPU) {
         tokenLossMultiplier = getRandomMultiplierWithRange(0,100);
@@ -63,7 +71,6 @@ app.get("/fetchData", (req, res) => {
 app.post("/battle", (req, res) => {
     var battleId = req.battleId;
     var avatarAttack = req.avatarAttack;
-    var isAvatarTurn = req.isAvatarTurn;
 
     var avatarHp = 0;
     var cpuHp = 0;
@@ -82,11 +89,10 @@ app.post("/battle", (req, res) => {
         cpuAttack = avatarAttack - 1;
     } 
     
-    results = battle(avatarHp, cpuHp, avatarAttack, cpuAttack, isAvatarTurn);
+    results = battle(avatarHp, cpuHp, avatarAttack, cpuAttack);
     // update db
-    
-    // send 200 success back to FE
-    res.send();
+    // send 200 success back to FE, with results
+    res.send(results);
 });
 
 // Starting the server on the 80 port
