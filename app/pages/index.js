f349c322f5e55dcc0e51fdc8fbb40e78070500be
Alyssa Yun <alyssay1226@gmail.com>
2022-01-15 12:20:26 -0800

    added navigatio

--- app/pages/index.js
+++ app/pages/index.js
@@ -3,10 +3,12 @@ import { Text, Flex } from "@chakra-ui/react";
 
 import { WalletContextProvider } from "../context/wallet";
 import { ConnectToWallets } from "../containers/wallet";
+import { NavigationBar } from "../containers/navigation";
 
 const Home = () => {
   return (
-    <Flex height="100vh">
+    <Flex height="100vh" flexDir={"column"} maxWidth={["100%", null, "640px"]} margin="auto">
+      <NavigationBar/>
       <Flex justifyContent="center" m="auto" flexDir={"column"}>
         <Flex justifyContent="center" alignItems={"center"} pb="4">
           <Text bgGradient='linear(to-l, #7928CA, #FF0080)' bgClip='text' fontSize="xl" fontWeight={"semibold"}>Select wallet</Text>
