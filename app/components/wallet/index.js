fe59421ba0674f9fa40ac773450976007f6a694b
Alyssa Yun <alyssay1226@gmail.com>
2022-01-14 18:14:08 -0800

    added frontend app

---
+++ app/components/wallet/index.js
@@ -0,0 +1,5 @@
+import { ConnectMetamaskButton } from "./ConnectMetamaskButton";
+
+export {
+  ConnectMetamaskButton,
+};
---
+++ app/containers/wallet/index.js
@@ -0,0 +1,5 @@
+import { ConnectToWallets } from "./ConnectToWallets";
+
+export {
+  ConnectToWallets,
+};
