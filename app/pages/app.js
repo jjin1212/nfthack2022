bb8223614cbe8f6d946f92d33c6d871c658c6f28
Alyssa Yun <alyssay1226@gmail.com>
2022-01-15 23:19:55 -0800

    added battle screen

--- app/pages/app.js
+++ app/pages/app.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Text, Flex } from "@chakra-ui/react";
+import { Image, Flex, Box, Text, Button, Center } from "@chakra-ui/react";
 
 import { WalletContextProvider } from "../context/wallet";
 import { NavigationBar } from "../containers/navigation";
@@ -9,7 +9,20 @@ const App = () => {
     <Flex height="100vh" flexDir={"column"} maxWidth={["100%", null, "640px"]} margin="auto">
       <NavigationBar/>
       <Flex justifyContent="center" m="auto" flexDir={"column"}>
-        App
+        <Box position={"relative"} width="640px" height="408px">
+          <Image src="/background.png" position={"absolute"} width="100%" height="auto"/>
+          <Image src="/character.png" position={"absolute"} width="100%" height="auto"/>
+          <Image src="/monster.png" position={"absolute"} width="100%" height="auto"/>
+          <Image src="/info-cards.png" position={"absolute"} width="100%" height="auto"/>
+          {/* <Image src="/attack-window.png" position={"absolute"} width="100%" height="auto"/> */}
+        </Box>
+        <Box p={3} width="100%" backgroundColor={"gray.500"} border="3px solid" borderRadius={"2xl"}>
+          <Center p={3}><Text color="black" fontWeight={"bold"}>Your turn</Text></Center>
+          <Flex direction="column">
+            <Button backgroundColor={"orange.400"} mb={3}>Attack</Button>
+            <Button backgroundColor={"orange.400"}>Weapon</Button>
+          </Flex>
+        </Box>
       </Flex>
     </Flex>
   );
