8488b681baa93813ea6c07812798f9d26ec8c331
Cody Wang <codywang@codys-mbp.lan>
2022-01-15 23:14:43 -0500

    deploy

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,31 +1,31 @@
 async function main() {
     // We get the contract to deploy
-    // const gameFactory = await ethers.getContractFactory("Game");
-    // const tokenFactory = await ethers.getContractFactory("TokenContract")
+    const gameFactory = await ethers.getContractFactory("Game");
+    const tokenFactory = await ethers.getContractFactory("TokenContract")
     const equipmentFactory = await ethers.getContractFactory("EquipmentContract")
-    // const avatarFactory = await ethers.getContractFactory("AvatarOwnership")
-    // const randGeneratorFactory = await ethers.getContractFactory("RandomNumberConsumer")
-    // token = await tokenFactory.deploy();
-    // await token.deployed()
+    const avatarFactory = await ethers.getContractFactory("AvatarOwnership")
+    const randGeneratorFactory = await ethers.getContractFactory("RandomNumberConsumer")
+    token = await tokenFactory.deploy();
+    await token.deployed()
     equipment = await equipmentFactory.deploy();
     await equipment.deployed()
-    // avatar = await avatarFactory.deploy(1000);
-    // await avatar.deployed()
-    // randGenerator = await randGeneratorFactory.deploy();
-    // await randGenerator.deployed()
-    // game = await gameFactory.deploy(
-    //     token.address,
-    //     equipment.address,
-    //     avatar.address,
-    //     randGenerator.address
-    // );
-    // await game.deployed()
+    avatar = await avatarFactory.deploy(1000);
+    await avatar.deployed()
+    randGenerator = await randGeneratorFactory.deploy();
+    await randGenerator.deployed()
+    game = await gameFactory.deploy(
+        token.address,
+        equipment.address,
+        avatar.address,
+        randGenerator.address
+    );
+    await game.deployed()
 
-    // console.log("Token deployed to:", token.address);
+    console.log("Token deployed to:", token.address);
     console.log("Equipment deployed to:", equipment.address);
-    // console.log("Avatar deployed to:", avatar.address);
-    // console.log("Rand generator deployed to:", randGenerator.address);
-    // console.log("Game deployed to:", game.address);
+    console.log("Avatar deployed to:", avatar.address);
+    console.log("Rand generator deployed to:", randGenerator.address);
+    console.log("Game deployed to:", game.address);
   }
   
   main()
