d160e7812075b5fb8f569b0f1cc7a2788765ee1c
Alyssa Yun <alyssay1226@gmail.com>
2022-01-15 21:02:59 -0800

    added avatar inventory UI

--- app/containers/inventory/AvatarAndEquipment.js
+++ app/containers/inventory/AvatarAndEquipment.js
@@ -25,9 +25,10 @@ export const AvatarAndEquipment = () => {
   return (
     <Flex width={"100%"} flexDir={"column"}>
       <Box py={[4, null, 6]}>
-        <Center>
+        <Center mb={3}>
           <Text bgGradient='linear(to-l, #7928CA, #FF0080)' bgClip='text' fontSize="xl" fontWeight={"semibold"}>You have {avatarState.count} Avatar NFT</Text>
         </Center>
+        <Image src="https://i.pinimg.com/736x/78/75/13/787513e1c0e5abe576350998ca659414.jpg" width={["90%", null, "auto"]} margin="auto" mb={[2, null, 5]} borderRadius="2xl" height={32}/>
       </Box>
       <Divider />
     </Flex>
