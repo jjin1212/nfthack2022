312fe664041ba13e71264272ac4a8c31051150dc
jjin <jjin@lyft.com>
2022-01-15 16:24:37 -0800

    uncomment

--- test/AvatarFactory.js
+++ test/AvatarFactory.js
@@ -1,17 +1,17 @@
-// const { expect } = require("chai");
+const { expect } = require("chai");
 
-// describe("AvatarFactory contract", function () {
-//     let avatarFactory;
+describe("AvatarFactory contract", function () {
+    let avatarFactory;
 
-//     beforeEach(async function() {
-//         const avatarFactoryFactory = await ethers.getContractFactory("ExposedAvatarFactory");
-//         avatarFactory = await avatarFactoryFactory.deploy();
-//     })
+    beforeEach(async function() {
+        const avatarFactoryFactory = await ethers.getContractFactory("ExposedAvatarFactory");
+        avatarFactory = await avatarFactoryFactory.deploy();
+    })
 
-//     it("Check _createAvatarAndGetId", async function () {
-//         const [owner, addr1] = await ethers.getSigners();
-//         let num = await avatarFactory.connect(addr1).createAvatarAndGetId.call()
-//         console.log(num);
-//         expect(await avatarFactory.createAvatarAndGetId.call()).to.equal(0);
-//     });
-// })
+    it("Check _createAvatarAndGetId", async function () {
+        const [owner, addr1] = await ethers.getSigners();
+        let num = await avatarFactory.connect(addr1).createAvatarAndGetId.call()
+        console.log(num);
+        expect(await avatarFactory.createAvatarAndGetId.call()).to.equal(0);
+    });
+})
