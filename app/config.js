237e56c50f90f95d09ac5b2949b575a288ea2721
Alyssa Yun <alyssay1226@gmail.com>
2022-01-15 16:21:27 -0800

    added config for equipment mint

--- app/config.js
+++ app/config.js
@@ -1,4 +1,5 @@
 export const config = {
   alchemy: process.env.NEXT_PUBLIC_ALCHEMY,
   contractAddress: process.env.NEXT_PUBLIC_CONTRACT_ADDRESS,
+  equipmentContractAddress: process.env.NEXT_PUBLIC_EQUIPMENT_CONTRACT_ADDRESS,
 };
